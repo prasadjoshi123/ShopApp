6950ad713d92807e64af2c88159ac365cd7e05a0
Prasad Joshi <prasad_joshi@horizonblue.com>
2020-05-12 22:44:15 +0530

    autologout added.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,6 +9,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
 bool get isAuth{
   return token != null;
@@ -48,6 +50,7 @@ String get userId{
       _token = responseData['idToken'];
       _userId = responseData['localId'];
       _expiryDate = DateTime.now().add(Duration(seconds: int.parse(responseData['expiresIn'],),),);
+      _autoLogout();
       notifyListeners();
     } catch (error) {
       throw error;
@@ -61,4 +64,25 @@ String get userId{
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout(){
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    if(_authTimer != null){
+      _authTimer.cancel();
+      _authTimer = null;
+    }
+    notifyListeners();
+  }
+
+  void _autoLogout(){
+    if(_authTimer != null){
+      _authTimer.cancel();
+    }
+
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer =  Timer(Duration(seconds: timeToExpiry), logout);
+  }
+
 }
